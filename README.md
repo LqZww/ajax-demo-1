08b4532a7900d527156bf15b281594d903a8a518
LqZww <951598077@qq.com>
2020-11-10 11:23:15 +0800

    update

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-## AJAX:异步的JavaScript和XML
-1.使用XMLHttpRequest发请求<br>
-2.服务器返回XML格式的字符串<br>
-3.JS解析XML，并更新局部页面
+## AJAX（异步的JavaScript和XML）
+1. 使用 XMLHttpRequest 发请求；
+2. 服务器返回 XML 格式的字符串；
+3. JS 解析 XML，并更新局部页面；
 
 ## 启动应用
+```
 node server.js 8888
+```
--- main.js
+++ main.js
@@ -1,34 +1,37 @@
+const { rejects } = require("assert")
+const { resolve } = require("path")
+
 myButton.addEventListener('click', (e) => {
-    let request = new XMLHttpRequest()
-    request.onreadystatechange = () => {
-        console.log('请求响应完毕')
-        if (request.readyState === 4) {
-            if (request.status >= 200 && request.status < 300) {
-                console.log('请求成功')
-                console.log(typeof request.responseText)
-                console.log(request.responseText)
-                let string = request.responseText
-                //把符合JSON语法的字符串转换成JS对应的值
-                //JSON.parse是浏览器提供的
-                let object = window.JSON.parse(string)
-                console.log(typeof object)
-                console.log(object)
-                console.log('object.note')
-                console.log(object.note)
-                console.log('object.note.from')
-                console.log(object.note.from)
-            } else if (request.status >= 400) {
-                console.log('请求失败')
-            }
-        }
+  let request = new XMLHttpRequest()
+  request.onreadystatechange = () => {
+    console.log('请求响应完毕')
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
+        console.log('请求成功')
+        console.log(typeof request.responseText)
+        console.log(request.responseText)
+        let string = request.responseText
+        // 把符合JSON语法的字符串转换成JS对应的值
+        // JSON.parse是浏览器提供的
+        let object = window.JSON.parse(string)
+        console.log(typeof object)
+        console.log(object)
+        console.log('object.note')
+        console.log(object.note)
+        console.log('object.note.from')
+        console.log(object.note.from)
+      } else if (request.status >= 400) {
+        console.log('请求失败')
+      }
     }
-    request.open('GET', '/xxx') //配置request
-    request.send()
+  }
+  request.open('GET', '/xxx') // 配置request
+  request.send()
 })
 
-//手写一个AJAX关键代码
+// 手写一个 AJAX 关键代码
 // let request = new XMLHttpRequest()
-// request.open('GET', '/xxx') //配置request
+// request.open('GET', '/xxx', true) //配置request
 // request.send()
 // request.onreadystatechange = () => {
 //     if (request.readyState === 4) {
@@ -39,3 +42,29 @@ myButton.addEventListener('click', (e) => {
 //     }
 // }
 
+// 封装
+// function ajax(url) {
+//   const p = new Promise((resolve, reject) => {
+//     const xhr = new XMLHttpRequest()
+//     xhr.open('GET', url, true)
+//     xhr.onreadystatechange = function () {
+//       if (xhr.readyState === 4) {
+//         if (xhr.status === 200) {
+//           resolve(JSON.parse(xhr.responseText))
+//         } else if (xhr.status === 404) {
+//           reject(new Error('404 not found'))
+//         }
+//       }
+//     }
+//     xhr.send(null)
+//   })
+//   return p
+// }
+// const url = '/xxx'
+// ajax(url)
+//   .then(res => {
+//     console.log(res);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   })
